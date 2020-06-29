43289966df7d1fe2a71629b105fc5905c5a2b785
luizgallas <50238463+luizgallas@users.noreply.github.com>
2020-06-29 10:15:02 -0500

    Delete config.js

--- config/config.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  consumer_key:         'Yh06KRINrclsg8cNo0yfXlonQ',
-  consumer_secret:      'r3t1GQvT2q8oscKJab2UNeGPrSGj5aFEhj9jDFJbUQWeUInmLr',
-  access_token:         '1276153671903055883-Se2Ux5QZe6vIVgSNY7Y2BMdr0jI0pb',
-  access_token_secret:  'tY8OHTNtJWhvimN5c7yJB4RUhbMFAQwS70zFvATAUxLOB',
-  timeout_ms:           60*1000,
-  strictSSL:            true,
-}
