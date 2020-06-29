b80e63f81909aea5509a2a64d88e4290605a4a5a
Luiz Gallas <luizgallas@outlook.com>
2020-06-29 12:04:57 -0500

    Ajustado scheduler de hora em hora e gitignore

--- bot.js
+++ bot.js
@@ -3,21 +3,23 @@ const moment = require('moment')
 var config = require('./config/config.js')
 const randomItem = require('random-item')
 var responseList = require('./constants/constants.js')
+var schedule = require('node-schedule')
 
 var T = new twit(config)
-var date = moment().subtract(1, 'hours')
+var date = moment().format("YYYY-MM-DD")
 
-T.get('search/tweets', { q: `@BotOraculo since:2020-06-29`, count: 100 }).then(function (response) {
-  console.log(response)
+var sched = schedule.scheduleJob({minute: 59}, function() {
+  T.get('search/tweets', { q: `@BotOraculo since:${date}`, count: 100 }).then(function (response) {
+    console.log(response)
 
-  response.data.statuses.forEach(tweet => {
-    var userName = tweet.user.screen_name;
-    var tweetId = tweet.id_str;
+    response.data.statuses.forEach(tweet => {
+      var userName = tweet.user.screen_name
+      var tweetId = tweet.id_str
 
-    T.post('statuses/update', { in_reply_to_status_id: tweetId, status: `@${userName} ${randomItem(responseList)}` },
-      (err, data, response) => {
-        resolve()
-        response.sendStatus(200)
-    }) 
-  });
-});
+      T.post('statuses/update', { in_reply_to_status_id: tweetId, status: `@${userName} ${randomItem(responseList)}` },
+        (err, data, response) => {
+          response.sendStatus(200)
+      }) 
+    })
+  })
+})
