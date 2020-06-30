72371da6e4ba9d382dd4e168efb75b83c9e925b9
Luiz Gallas <luizgallas@outlook.com>
2020-06-30 18:10:42 -0300

    Ajustado tratamento de data para sempre usar utc

--- validations/validations.js
+++ validations/validations.js
@@ -2,10 +2,10 @@ const moment = require('moment')
 const { answerOptionsPT, answerOptionsEN } = require('.././constants/constants.js')
 
 const validateDate = tweet => {
-  const tweetDate = moment(tweet.created_at, "ddd MMM DD HH:mm:ss ZZ yyyy");
+  const tweetDate = moment.utc(tweet.created_at, "ddd MMM DD HH:mm:ss ZZ yyyy");
   console.log(tweetDate)
 
-  const hours = moment.duration(moment().diff(tweetDate)).asHours()
+  const hours = moment.duration(moment.utc().diff(tweetDate)).asHours()
   console.log(hours)
   return hours >= 0 && hours <= 1;
 }
